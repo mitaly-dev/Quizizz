deaad9409418871a93543b39456d015cd0d52713
mitaly-dev <mitalyy.dev@gmail.com>
2022-10-10 08:21:14 -0700

    react app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className='text-red-600'>this is hedaer</h3>
+    </div>
+  );
+}
+
+export default App;
